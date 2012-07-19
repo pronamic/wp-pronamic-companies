fe5f19d0c9df4ce3fb2e4924bb3df8c849fe186c
Remco Tolsma <info@remcotolsma.nl>
2012-07-19 20:41:49 +0200

    Added correct version number.

--- pronamic-companies.php
+++ pronamic-companies.php
@@ -4,7 +4,7 @@ Plugin Name: Pronamic Companies
 Plugin URI: http://pronamic.eu/wordpress/companies/
 Description: This plugin add some basic company directory functionality to WordPress
 
-Version: 1.0
+Version: 0.1
 Requires at least: 3.0
 
 Author: Pronamic
