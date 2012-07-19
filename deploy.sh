e8e5b9cdc7ce71874272a712f609cc2c96965a36
Remco Tolsma <info@remcotolsma.nl>
2012-07-19 20:57:14 +0200

    Change SVN username.

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/pronamic-companies/" # Remote SVN repo on wordpress.org, with no trailing slash
-SVNUSER="pronamic" # your svn username
+SVNUSER="remcotolsma" # your svn username
 
 
 # Let's begin...
