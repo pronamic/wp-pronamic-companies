02d7231c08ce023b460ad6a51264679184d4e69e
Remco Tolsma <info@remcotolsma.nl>
2012-10-04 16:16:54 +0200

    Improved and added template functions.

--- includes/template.php
+++ includes/template.php
@@ -1,23 +1,21 @@
 <?php
 
+//////////////////////////////////////////////////
+
 /**
  * Get the company address
  * 
  * @return string
  */
 function pronamic_company_get_address( $post_id = null ) {
-	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
-
-	return get_post_meta( $post_id, '_pronamic_company_address', true );
+	return pronamic_company_get_meta( '_pronamic_company_address', $post_id );
 }
 
 /**
  * Check if company has address
  */
 function pronamic_company_has_address( $post_id = null ) {
-	$address = pronamic_company_get_address( $post_id );
-
-	return !empty( $address );
+	return pronamic_company_has_meta( '_pronamic_company_address', $post_id );
 }
 
 /**
@@ -35,18 +33,14 @@ function pronamic_company_the_address( $post_id = null ) {
  * @return string
  */
 function pronamic_company_get_postal_code( $post_id = null ) {
-	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
-
-	return get_post_meta( $post_id, '_pronamic_company_postal_code', true );
+	return pronamic_company_get_meta( '_pronamic_company_postal_code', $post_id );
 }
 
 /**
  * Check if company has postal code
  */
 function pronamic_company_has_postal_code( $post_id = null ) {
-	$postal_code = pronamic_company_get_postal_code( $post_id );
-
-	return !empty( $postal_code );
+	return pronamic_company_has_meta( '_pronamic_company_postal_code', $post_id );
 }
 
 /**
@@ -64,18 +58,14 @@ function pronamic_company_the_postal_code( $post_id = null ) {
  * @return string
  */
 function pronamic_company_get_city( $post_id = null ) {
-	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
-
-	return get_post_meta( $post_id, '_pronamic_company_city', true );
+	return pronamic_company_get_meta( '_pronamic_company_city', $post_id );
 }
 
 /**
  * Check if company has city
  */
 function pronamic_company_has_city( $post_id = null ) {
-	$city = pronamic_company_get_city( $post_id );
-
-	return !empty( $city );
+	return pronamic_company_has_meta( '_pronamic_company_city', $post_id );
 }
 
 /**
@@ -93,18 +83,14 @@ function pronamic_company_the_city( $post_id = null ) {
  * @return string
  */
 function pronamic_company_get_phone_number( $post_id = null ) {
-	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
-
-	return get_post_meta( $post_id, '_pronamic_company_phone_number', true );
+	return pronamic_company_get_meta( '_pronamic_company_phone_number', $post_id );
 }
 
 /**
  * Check if company has phone number
  */
 function pronamic_company_has_phone_number( $post_id = null ) {
-	$phone_number = pronamic_company_get_phone_number( $post_id );
-
-	return !empty( $phone_number );
+	return pronamic_company_has_meta( '_pronamic_company_phone_number', $post_id );
 }
 
 /**
@@ -122,18 +108,14 @@ function pronamic_company_the_phone_number( $post_id = null ) {
  * @return string
  */
 function pronamic_company_get_fax_number( $post_id = null ) {
-	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
-
-	return get_post_meta( $post_id, '_pronamic_company_fax_number', true );
+	return pronamic_company_get_meta( '_pronamic_company_fax_number', $post_id );
 }
 
 /**
  * Check if company has fax number
  */
 function pronamic_company_has_fax_number( $post_id = null ) {
-	$fax_number = pronamic_company_get_phone_number( $post_id );
-
-	return !empty( $fax_number );
+	return pronamic_company_has_meta( '_pronamic_company_fax_number', $post_id );
 }
 
 /**
@@ -151,18 +133,14 @@ function pronamic_company_the_fax_number( $post_id = null ) {
  * @return string
  */
 function pronamic_company_get_email( $post_id = null ) {
-	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
-
-	return get_post_meta( $post_id, '_pronamic_company_email', true );
+	return pronamic_company_get_meta( '_pronamic_company_email', $post_id );
 }
 
 /**
  * Check if company has email
  */
 function pronamic_company_has_email( $post_id = null ) {
-	$email = pronamic_company_get_email( $post_id );
-
-	return !empty( $email );
+	return pronamic_company_has_meta( '_pronamic_company_email', $post_id );
 }
 
 /**
@@ -180,18 +158,14 @@ function pronamic_company_the_email( $post_id = null ) {
  * @return string
  */
 function pronamic_company_get_website( $post_id = null ) {
-	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
-
-	return get_post_meta( $post_id, '_pronamic_company_website', true );
+	return pronamic_company_get_meta( '_pronamic_company_website', $post_id );
 }
 
 /**
  * Check if company has website
  */
 function pronamic_company_has_website( $post_id = null ) {
-	$website = pronamic_company_get_website( $post_id );
-
-	return !empty( $website );
+	return pronamic_company_has_meta( '_pronamic_company_website', $post_id );
 }
 
 /**
@@ -202,4 +176,82 @@ function pronamic_company_the_website( $post_id = null ) {
 }
 
 //////////////////////////////////////////////////
+
+/**
+ * Get the company Twitter
+ * 
+ * @return string
+ */
+function pronamic_company_get_twitter( $post_id = null ) {
+	return pronamic_company_get_meta( '_pronamic_company_twitter', $post_id );
+}
+
+/**
+ * Check if company has Twitter
+ */
+function pronamic_company_has_twitter( $post_id = null ) {
+	return pronamic_company_has_meta( '_pronamic_company_twitter', $post_id );
+}
+
+/**
+ * Output the company Twitter
+ */
+function pronamic_company_the_twitter( $post_id = null ) {
+	echo pronamic_company_get_twitter( $post_id );
+}
+
+//////////////////////////////////////////////////
+
+/**
+ * Get the company Facebook
+ * 
+ * @return string
+ */
+function pronamic_company_get_facebook( $post_id = null ) {
+	return pronamic_company_get_meta( '_pronamic_company_facebook', $post_id );
+}
+
+/**
+ * Check if company has Facebook
+ */
+function pronamic_company_has_facebook( $post_id = null ) {
+	return pronamic_company_has_meta( '_pronamic_company_facebook', $post_id );
+}
+
+/**
+ * Output the company Facebook
+ */
+function pronamic_company_the_facebook( $post_id = null ) {
+	echo pronamic_company_get_facebook( $post_id );
+}
+
+//////////////////////////////////////////////////
+
+/**
+ * Get the company LinkedIN
+ * 
+ * @return string
+ */
+function pronamic_company_get_linkedin( $post_id = null ) {
+	return pronamic_company_get_meta( '_pronamic_company_linkedin', $post_id );
+}
+
+/**
+ * Check if company has Facebook
+ */
+function pronamic_company_has_linkedin( $post_id = null ) {
+	return pronamic_company_has_meta( '_pronamic_company_linkedin', $post_id );
+}
+
+/**
+ * Output the company Facebook
+ */
+function pronamic_company_the_linkedin( $post_id = null ) {
+	echo pronamic_company_get_linkedin( $post_id );
+}
+
+//////////////////////////////////////////////////
+
+
+
 
