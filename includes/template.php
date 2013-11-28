bc73b209084a13edecd8508346719711e220e0b5
Leon Rowland <leon@rowland.nl>
2013-11-28 22:41:06 +0100

    Addition of the method to get the contact person line from the company meta.

--- includes/template.php
+++ includes/template.php
@@ -1,5 +1,13 @@
 <?php
 
+function pronamic_company_get_contact( $post_id = null ) {
+    return pronamic_company_get_meta( '_pronamic_company_contact', $post_id );
+}
+
+function pronamic_company_contact( $post_id = null ) {
+    echo pronamic_company_get_contact( $post_id );
+}
+
 /**
  * Get the company address
  * 
